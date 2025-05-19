f50ce529a8b0cb85d7aebc49fe643df771e08b8a
Rafaela Vieira Cabral <rafaelavieiracabral04@gmail.com>
2025-05-19 13:16:17 -0300

    feat: implementação da função que separa números pares

--- src/main/kotlin/NumeroPar.kt
+++ src/main/kotlin/NumeroPar.kt
@@ -1,2 +1,33 @@
 package org.example
 
+//Crie uma função que receba um array de inteiros e retorne apenas os números pares.
+
+//Com lib
+fun numeroPar(numeros: Array<Int>): Array<Int> {
+    return numeros.filter { it % 2 == 0 }.toTypedArray()
+}
+
+//Sem lib
+//fun numeroPar(numeros: Array<Int>): Array<Int>{
+//    val numerosPares = mutableListOf<Int>()
+//
+//    for(numero in numeros){
+//        if (numero % 2==0){
+//            numerosPares.add(numero)
+//        }
+//    }
+//
+//    return numerosPares.toTypedArray()
+//
+//}
+fun main() {
+    print("Digite uma lista de números, separados por vírgula:")
+
+    val entrada = readLine()?.toString()?: ""
+    val numeros = entrada.split(",").map { it.trim().toInt()}.toTypedArray()
+
+    val resultado = numeroPar(numeros)
+
+    println("Os números pares são: ${resultado.joinToString(", ")}")
+
+}
