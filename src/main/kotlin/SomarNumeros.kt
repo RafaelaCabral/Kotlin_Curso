4982cff9968c3d3c9937b85e7f0b21ce96228c2b
RafaelaCabral <rafaelavieiracabral04@gmail.com>
2025-05-18 21:21:18 -0300

    refactor: melhoria na função de somar os números de uma lista

--- src/main/kotlin/SomarNumeros.kt
+++ src/main/kotlin/SomarNumeros.kt
@@ -1,12 +1,18 @@
 package org.example
 
+//Com lib
 fun somarNumeros(numeros: Array<Double>): Double{
-    var soma = 0.0
-    for(numero in numeros){
-        soma += numero
-    }
+    var soma = numeros.sum()
     return soma
 }
+//Sem lib
+//fun somarNumeros(numeros: Array<Double>): Double{
+//    var soma = 0.0
+//    for(numero in numeros){
+//        soma += numero
+//    }
+//    return soma
+//}
 fun main() {
     print("Digite uma lista de numeros separadas por vírgula. Ex. 1, 2, 3.32: ")
     val entrada = readLine()?: " "
