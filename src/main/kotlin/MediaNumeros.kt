a45c68950ccc7c51144a0782625396afb79f2eaa
RafaelaCabral <rafaelavieiracabral04@gmail.com>
2025-05-18 21:21:54 -0300

    feat: implementação da função para cálcular a média dos números em uma lista

--- src/main/kotlin/MediaNumeros.kt
+++ src/main/kotlin/MediaNumeros.kt
@@ -1,2 +1,30 @@
 package org.example
 
+//Com lib
+fun mediaNumeros(numeros: Array<Double>): Double {
+    return numeros.average()
+}
+
+//Sem lib
+//fun mediaNumeros(numeros: Array<Double>): Double{
+//    val tamanhoLista = numeros.size
+//    var soma = 0.0
+//    for(numero in numeros){
+//        soma += numero
+//
+//    }
+//    val media = soma / tamanhoLista
+//    return media
+//}
+fun main() {
+    print("Digite uma lista de numeros separadas por vírgula. Ex. 1, 2, 3.32: ")
+    val entrada = readLine()?: " "
+
+
+    val numeros = entrada.split(",").map { it.trim().toDouble() }.toTypedArray()
+
+    val resultado = mediaNumeros(numeros)
+
+    println("A média é: ${resultado}")
+
+}
