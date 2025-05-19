1f7eccf59514667c075d9f44c82c6bcb314d72a2
RafaelaCabral <rafaelavieiracabral04@gmail.com>
2025-05-18 21:10:46 -0300

    feat: implementação da função para somar números de uma string

---
+++ src/main/kotlin/SomarNumeros.kt
@@ -0,0 +1,21 @@
+package org.example
+
+fun somarNumeros(numeros: Array<Double>): Double{
+    var soma = 0.0
+    for(numero in numeros){
+        soma += numero
+    }
+    return soma
+}
+fun main() {
+    print("Digite uma lista de numeros separadas por vírgula. Ex. 1, 2, 3.32: ")
+    val entrada = readLine()?: " "
+
+
+    val numeros = entrada.split(",").map { it.trim().toDouble() }.toTypedArray()
+
+    val resultado = somarNumeros(numeros)
+
+    println("A soma total é: ${resultado}")
+
+}
